f9cb97c0b1d2f33776a49c464690c0edb10061ad
Aylén Bavosa Castro <aylucastro@gmail.com>
2020-03-09 23:21:38 -0300

    Modificación de carpeta INCLUDES

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,17 @@
     </main>
     
     <footer> 
-            <p>Barras Móviles Refill - Buenos Aires - Argentina</p>
-            <p>celular: 15...... / mail: refill.barramovil@gmail.com</p>
-            <p>Instagram: &nbsp;<a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_Instagram_381384.png" alt="" class="instagram">&nbsp; refill_barramovil</a></p>   
+            <hr>
+            <h3>ENCONTRANOS EN</h3>
+            <p><a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_Instagram_381384.png" alt="" class="instagram"> &nbsp; refill_barramovil</a></p> <!-- Instagram -->
+            <p><a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_square-facebook_317727.png" alt="" class="instagram"> &nbsp; refill_barramovil</a></p> <!-- Facebook | poner el link a la página de fb, por ahora lleva a instagram -->
+            <br>
+            <hr>
+            <h3>CONTACTO</h3>
+            <p><img src="./icon/iconfinder_mobile_2431356.png" alt="" class="instagram"> &nbsp;  +54911...... </p>  <!-- Teléfono -->
+            <p><img src="./icon/iconfinder_mail_1055030.png" alt="" class="instagram"> &nbsp; refill.barramovil@gmail.com</p>  <!-- Mail -->
+            <p><img src="./icon/iconfinder_Flag_2376767.png" alt="" class="instagram"> &nbsp; Buenos Aires - Argentina</p>  <!-- Locación -->
+               
     </footer>
 
 </body>
