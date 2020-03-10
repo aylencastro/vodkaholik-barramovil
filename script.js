a1783ceb5367f34d4376b8c1c1f2eee096ebdf60
Aylén Bavosa Castro <aylucastro@gmail.com>
2020-03-10 18:25:27 -0300

    actualiado del javascript para la versión mobile

--- script.js
+++ script.js
@@ -4,8 +4,12 @@
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header_mobile").style.top = "0";
+    document.querySelector(".consult").style.left = "0";
+    document.querySelector(".wp").style.left = "0";
   } else {
     document.getElementById("header_mobile").style.top = "-11vh";
+    document.querySelector(".consult").style.left = "-25vw";
+    document.querySelector(".wp").style.left = "-25vw";
   }
   prevScrollpos = currentScrollPos;
 }
