749bca8392ac526ffe790419015c4f87a85b5725
Aylén Bavosa Castro <aylucastro@gmail.com>
2020-03-11 18:29:29 -0300

    actualización del footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,14 +4,14 @@
         <div class="footer-text">
             <hr>
             <h3>ENCONTRANOS EN</h3>
-            <span><a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_Instagram_381384.png" alt="" class="instagram"><p>&nbsp; refill_barramovil</a></p></span> <!-- Instagram -->
-            <span><a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_square-facebook_317727.png" alt="" class="instagram"><p> &nbsp; refill_barramovil</a></p></span> <!-- Facebook | poner el link a la página de fb, por ahora lleva a instagram -->
+            <span><a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_Instagram_381384.png" alt="" class="footer-contacto"><p>&nbsp; refill_barramovil</p></a></span> <!-- Instagram -->
+            <span><a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_square-facebook_317727.png" alt="" class="footer-contacto"><p> &nbsp; refill_barramovil</p></a></span> <!-- Facebook | poner el link a la página de fb, por ahora lleva a instagram -->
             <br>
             <hr>
             <h3>CONTACTO</h3>
-            <span><img src="./icon/iconfinder_mobile_2431356.png" alt="" class="instagram"><p> &nbsp;  +54911...... </p></span>  <!-- Teléfono -->
-            <span><img src="./icon/iconfinder_mail_1055030.png" alt="" class="instagram"><p> &nbsp; refill.barramovil@gmail.com</p></span>  <!-- Mail -->
-            <span><img src="./icon/iconfinder_Flag_2376767.png" alt="" class="instagram"><p>&nbsp; Buenos Aires - Argentina</p></span>  <!-- Locación -->
+            <span><img src="./icon/iconfinder_mobile_2431356.png" alt="" class="footer-contacto"><p> &nbsp;  +54911...... </p></span>  <!-- Teléfono -->
+            <span><img src="./icon/iconfinder_mail_1055030.png" alt="" class="footer-contacto"><p> &nbsp; refill.barramovil@gmail.com</p></span>  <!-- Mail -->
+            <span><img src="./icon/iconfinder_Flag_2376767.png" alt="" class="footer-contacto"><p>&nbsp; Buenos Aires - Argentina</p></span>  <!-- Locación -->
         </div>     
     </footer>
 
