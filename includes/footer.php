dcb1684aabed5dcf358294772eab91c8e7588d4b
Aylén Bavosa Castro <aylucastro@gmail.com>
2021-01-16 20:27:06 -0300

    pequeñas modificaciones en carpeta 'includes'

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,16 @@
-    </main>
+</main>
     
     <footer> 
         <div class="footer-text">
             <hr>
             <h3>ENCONTRANOS EN</h3>
-            <span><a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_Instagram_381384.png" alt="" class="footer-contacto"><p>&nbsp; refill_barramovil</p></a></span> <!-- Instagram -->
-            <span><a href="https://www.instagram.com/refill_barramovil/" target="_blank"><img src="./icon/iconfinder_square-facebook_317727.png" alt="" class="footer-contacto"><p> &nbsp; refill_barramovil</p></a></span> <!-- Facebook | poner el link a la página de fb, por ahora lleva a instagram -->
+            <span><a href="https://www.instagram.com/" target="_blank"><img src="./icon/iconfinder_Instagram_381384.png" alt="" class="footer-contacto"><p>&nbsp; vodkaholic_barramovil</p></a></span> <!-- Instagram -->
+            <span><a href="https://www.facebook.com/" target="_blank"><img src="./icon/iconfinder_square-facebook_317727.png" alt="" class="footer-contacto"><p> &nbsp; vodkaholic_barramovil</p></a></span> <!-- Facebook -->
             <br>
             <hr>
             <h3>CONTACTO</h3>
             <span><img src="./icon/iconfinder_mobile_2431356.png" alt="" class="footer-contacto"><p> &nbsp;  +54911...... </p></span>  <!-- Teléfono -->
-            <span><img src="./icon/iconfinder_mail_1055030.png" alt="" class="footer-contacto"><p> &nbsp; refill.barramovil@gmail.com</p></span>  <!-- Mail -->
+            <span><img src="./icon/iconfinder_mail_1055030.png" alt="" class="footer-contacto"><p> &nbsp; vodkaholic.barramovil@gmail.com</p></span>  <!-- Mail -->
             <span><img src="./icon/iconfinder_Flag_2376767.png" alt="" class="footer-contacto"><p>&nbsp; Buenos Aires - Argentina</p></span>  <!-- Locación -->
         </div>     
     </footer>
--- includes/head.php
+++ includes/head.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./styles/style.css">
-    <script src="script.js"></script>
+    <script src="./scripts/script.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Advent+Pro|BenchNine|Dancing+Script|Kaushan+Script|Merienda|Pragati+Narrow|Tenor+Sans&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" href="./icon/iconfinder_anchor_3521344.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./icon/iconfinder_Cocktail_2376769.ico" type="image/x-icon">
     <title><?php echo TITLE; ?></title>
 </head>
 
